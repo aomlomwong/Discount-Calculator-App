910d5d75eb8988e736bf4b622d823e1c6ef3f547
aomlomwong <71165296+aomlomwong@users.noreply.github.com>
2021-01-19 16:47:01 +0700

    init

--- DiscountCalculator/app/src/main/java/th/ac/su/ict/discountcalculator/MainActivity.kt
+++ DiscountCalculator/app/src/main/java/th/ac/su/ict/discountcalculator/MainActivity.kt
@@ -2,10 +2,44 @@ package th.ac.su.ict.discountcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.CheckBox;
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val oriPrice = findViewById<EditText>(R.id.oriprice)
+        val perPrice = findViewById<EditText>(R.id.perPrice)
+        val totalText = findViewById<TextView>(R.id.totalText)
+        val btCal = findViewById<Button>(R.id.btcal)
+
+        btCal.setOnClickListener(){
+
+            var originalPrice:Int = oriPrice.text.toString().toInt()
+            var percentPrice:Int = perPrice.text.toString().toInt()
+
+            var totalPrice:Int =(originalPrice*percentPrice)/100
+            totalText.text = totalPrice.toString()
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
 }
